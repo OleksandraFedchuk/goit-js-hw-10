9fc954e17494d91b70ddea8238ede9208249e60e
Oleksandra <sunnyem@seznam.cz>
2024-11-19 04:10:21 +0100

    commit

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,62 +1,124 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+    validateDate();
+  },
+
+};
+
+flatpickr('#datetime-picker' , options);
+
 const elements = {
   timer: document.querySelector('.timer'),
   days : document.querySelector('span[data-days]'),
   hours: document.querySelector('span[data-hours]'),
   minutes : document.querySelector('span[data-minutes]'),
   seconds : document.querySelector('span[data-secondrs]'),
-  button: document.querySelector('button[data-start]')
+  button: document.querySelector('button[data-start]'),
+  picker: document.querySelector('#datetime-picker'),
 }
 
 let userSelectedDate = null ; 
-elements.button.disabled = true;
+
+function validateDate(){
+  if (userSelectedDate < Date.now()){
+    iziToast.show({
+      message: "Please choose a date in the future",
+      position: "topCenter",
+      closeOnEscape: true,
+      closeOnClick: true,
+    });
+    elements.button = true;
+  } else {
+    elements.button = false;
+  }
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
 
 function addLeadingZero(value) {
   return String(value).padStart(2, '0');
 }
 
+function updateTimerFace({days, hours, minutes, seconds}) {
+  elements.days = addLeadingZero(days);
+  elements.hours = addLeadingZero(hours);
+  elements.minutes = addLeadingZero(minutes);
+  elements.seconds = addLeadingZero(seconds);
+};
+
+let countInterval = null; 
+
+function startCount() {
+  countInterval = setInterval(() => {
+const delta = userSelectedDate - Date.now() ;
+
+if (delta <= 0){
+  clearInterval(countInterval);
+  updateTimerFace({ days: "00", hours: "00", seconds: "00"});
+  elements.button = true; 
+  elements.picker = false;
+  return;
+}
+const time = convertMs(delta);
+updateTimerFace(time);
+  }, 1000);
+}
+
+elements.button.addEventListener("click", () => {
+if(!userSelectedDate)
+  return;
+
+if(countInterval){
+  clearInterval(countInterval);
+}
+
+elements.picker = true ;
+elements.button = true;
+
+startCount();
+  
+});
 
 
 
-const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
-  
-  function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
-  
-  console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-  console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-  console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-  
-  import flatpickr from "flatpickr";
-  import "flatpickr/dist/flatpickr.min.css";
 
-  import iziToast from "izitoast";
-  import "izitoast/dist/css/iziToast.min.css";
 
-  elements.button.addEventListener("click" , elements.timer.start);
+
+
 
   
-  
+
+
+
+
+
+
+
+
+
+
+
+
