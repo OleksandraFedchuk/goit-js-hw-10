fec72d580242d4e756b6116a6a7aa7c1569550b0
OleksandraFedchuk <125163547+OleksandraFedchuk@users.noreply.github.com>
2025-01-11 15:08:24 +0000

    Deploying to gh-pages from @ OleksandraFedchuk/goit-js-hw-10@f198c687b87f87cfed296bfa66fecb51b31c3345 🚀

---
+++ 2-snackbar.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */const i=new Promise((e,s)=>{setTimeout(()=>{isSucess?e("This function is resolved"):s("This function is rejected")},2e3)});console.log(i);
+//# sourceMappingURL=2-snackbar.js.map
