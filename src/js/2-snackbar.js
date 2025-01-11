ce3504e65e5d0dcd9494bf27a03fe997878bb2d8
Oleksandra <sunnyem@seznam.cz>
2025-01-11 16:20:21 +0100

    commit

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -8,5 +8,14 @@ const promise = new Promise((resolve, reject) => {
     }, 2000
     )
 })
-
+const isSucess = false ;
 console.log(promise);
+
+promise.then(value => {console.log(value);
+}).catch(error => {console.log(error)}
+).finally(() => console.log(promise)
+)
+
+const btn = document.querySelector('.button');
+console.log(btn);
+
